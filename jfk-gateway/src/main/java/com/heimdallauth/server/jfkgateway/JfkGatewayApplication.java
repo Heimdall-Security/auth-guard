4a4c7181dd4516d3587c5281f69a9ba3d4de23ea
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-01-14 23:30:15 -0700

    init(gateway): Commit to create project files for Spring Gateway

---
+++ jfk-gateway/src/main/java/com/heimdallauth/server/jfkgateway/JfkGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.heimdallauth.server.jfkgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JfkGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JfkGatewayApplication.class, args);
+    }
+
+}
---
+++ jfk-gateway/src/test/java/com/heimdallauth/server/jfkgateway/JfkGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heimdallauth.server.jfkgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JfkGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
