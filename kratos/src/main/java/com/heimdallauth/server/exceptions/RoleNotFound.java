8282011db121e6b9f39b0087db3e25761b7505c6
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-01-18 17:29:09 -0700

    improve(Core): Updated to include the role id for role not found exception

--- kratos/src/main/java/com/heimdallauth/server/exceptions/RoleNotFound.java
+++ kratos/src/main/java/com/heimdallauth/server/exceptions/RoleNotFound.java
@@ -1,7 +1,13 @@
 package com.heimdallauth.server.exceptions;
 
 public class RoleNotFound extends RuntimeException {
+    public String roleId;
     public RoleNotFound(String message) {
         super(message);
+    }
+
+    public RoleNotFound(String message, String roleId) {
+        super(message);
+        this.roleId = roleId;
     }
 }
