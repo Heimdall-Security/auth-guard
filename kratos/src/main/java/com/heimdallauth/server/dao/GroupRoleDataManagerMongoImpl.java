e6fcd56030669340298661221583800419360b4a
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-01-18 18:19:54 -0700

    improve(core): update to merge implementations for Groups and roles on MongoDB

--- kratos/src/main/java/com/heimdallauth/server/dao/GroupRoleDataManagerMongoImpl.java
+++ kratos/src/main/java/com/heimdallauth/server/dao/GroupRoleDataManagerMongoImpl.java
@@ -1,7 +1,9 @@
 package com.heimdallauth.server.dao;
 
 import com.heimdallauth.server.commons.models.GroupModel;
+import com.heimdallauth.server.commons.models.RoleModel;
 import com.heimdallauth.server.datamanagers.GroupDataManager;
+import com.heimdallauth.server.datamanagers.RoleDataManager;
 import com.heimdallauth.server.exceptions.GroupNotFound;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -14,13 +16,33 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public class GroupsMongoDataManager implements GroupDataManager {
+public class GroupRoleDataManagerMongoImpl implements GroupDataManager, RoleDataManager {
     private final MongoTemplate mongoTemplate;
     private static final String COLLECTION_GROUPS = "groups-collection";
     private static final String COLLECTION_GROUP_ROLE_MEMBERSHIPS = "group-role-memberships-collection";
+    private static final String ROLES_COLLECTION = "role-collection";
 
-    public GroupsMongoDataManager(MongoTemplate mongoTemplate) {
+    public GroupRoleDataManagerMongoImpl(MongoTemplate mongoTemplate) {
         this.mongoTemplate = mongoTemplate;
+    }
+
+    private static RoleDocument convertToRoleDocument(RoleModel roleModel){
+        return RoleDocument.builder()
+                .id(roleModel.getId())
+                .roleName(roleModel.getRoleName())
+                .roleDescription(roleModel.getRoleDescription())
+                .createdOn(roleModel.getCreatedOn())
+                .lastUpdatedOn(roleModel.getLastUpdatedOn())
+                .build();
+    }
+    private static RoleModel convertToRoleModel(RoleDocument roleDocument){
+        return RoleModel.builder()
+                .id(roleDocument.getId())
+                .roleName(roleDocument.getRoleName())
+                .roleDescription(roleDocument.getRoleDescription())
+                .createdOn(roleDocument.getCreatedOn())
+                .lastUpdatedOn(roleDocument.getLastUpdatedOn())
+                .build();
     }
 
     public static GroupModel convertGroupDocumentToGroupModel(GroupDocument groupDocument) {
@@ -51,11 +73,11 @@ public class GroupsMongoDataManager implements GroupDataManager {
     @Override
     public List<GroupModel> createGroup(List<GroupModel> groupModels) {
         List<GroupDocument> documentsToSave = groupModels.stream()
-                .map(GroupsMongoDataManager::convertGroupModelToGroupDocument)
+                .map(GroupRoleDataManagerMongoImpl::convertGroupModelToGroupDocument)
                 .toList();
         List<GroupDocument> savedDocuments = (List<GroupDocument>) this.mongoTemplate.insertAll(documentsToSave);
         return savedDocuments.stream()
-                .map(GroupsMongoDataManager::convertGroupDocumentToGroupModel)
+                .map(GroupRoleDataManagerMongoImpl::convertGroupDocumentToGroupModel)
                 .toList();
     }
 
@@ -117,14 +139,14 @@ public class GroupsMongoDataManager implements GroupDataManager {
                 Criteria.where("groupDescription").regex(searchTerm, "i")
         ));
         return Optional.ofNullable(this.mongoTemplate.findOne(searchDBQuery, GroupDocument.class, COLLECTION_GROUPS))
-                .map(GroupsMongoDataManager::convertGroupDocumentToGroupModel);
+                .map(GroupRoleDataManagerMongoImpl::convertGroupDocumentToGroupModel);
     }
 
     @Override
     public List<GroupModel> getAllGroups() {
         List<GroupDocument> allGroups = this.mongoTemplate.findAll(GroupDocument.class, COLLECTION_GROUPS);
         return allGroups.stream()
-                .map(GroupsMongoDataManager::convertGroupDocumentToGroupModel)
+                .map(GroupRoleDataManagerMongoImpl::convertGroupDocumentToGroupModel)
                 .toList();
     }
 
@@ -146,5 +168,45 @@ public class GroupsMongoDataManager implements GroupDataManager {
         this.mongoTemplate.insert(newRoleMemberships, COLLECTION_GROUP_ROLE_MEMBERSHIPS);
         GroupDocument updatedGroupDocument  = Optional.ofNullable(this.mongoTemplate.findOne(selectGroupQuery, GroupDocument.class, COLLECTION_GROUPS)).orElseThrow(() -> new GroupNotFound("The group not found", groupId));
         return convertGroupDocumentToGroupModel(updatedGroupDocument);
+    }
+
+    @Override
+    public RoleModel createRole(String roleName, String roleDescription) {
+        RoleDocument roleDocumentToSave = RoleDocument.builder()
+                .roleName(roleName)
+                .roleDescription(roleDescription)
+                .createdOn(Instant.now())
+                .lastUpdatedOn(Instant.now())
+                .build();
+        RoleDocument savedRoleDocument = mongoTemplate.save(roleDocumentToSave, ROLES_COLLECTION);
+        return convertToRoleModel(savedRoleDocument);
+    }
+
+    @Override
+    public Optional<RoleModel> getRoleById(String roleId) {
+        Query selectRoleFromId = Query.query(Criteria.where("id").is(roleId));
+        return Optional.ofNullable(mongoTemplate.findOne(selectRoleFromId, RoleDocument.class, ROLES_COLLECTION)).map(GroupRoleDataManagerMongoImpl::convertToRoleModel);
+    }
+
+    @Override
+    public List<RoleModel> getRolesByIds(List<String> ids) {
+        Query selectRolesByIds = Query.query(Criteria.where("id").in(ids));
+        return mongoTemplate.find(selectRolesByIds, RoleDocument.class, ROLES_COLLECTION).stream().map(GroupRoleDataManagerMongoImpl::convertToRoleModel).toList();
+    }
+
+    @Override
+    public Optional<RoleModel> searchRoleUsingRoleNameOrRoleDescription(String searchTerm) {
+        Query searchQuery = Query.query(Criteria.where("roleName").regex(searchTerm, "i").orOperator(Criteria.where("roleDescription").regex(searchTerm,"i")));
+        return Optional.ofNullable(mongoTemplate.findOne(searchQuery, RoleDocument.class, ROLES_COLLECTION)).map(GroupRoleDataManagerMongoImpl::convertToRoleModel);
+    }
+
+    @Override
+    public Optional<RoleModel> updateRole(String roleId, String roleName, String roleDescription) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<RoleModel> getAllRoles() {
+        return this.mongoTemplate.findAll(RoleDocument.class, ROLES_COLLECTION).stream().map(GroupRoleDataManagerMongoImpl::convertToRoleModel).toList();
     }
 }
