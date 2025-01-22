adda696c9670cfc20ee4c7a45a617975c96c2520
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-01-22 16:23:42 -0700

    improve(ConfigurationSet): Implement a constant class to hold the collection names

---
+++ bifrost/src/main/java/com/heimdallauth/server/constants/MongoCollectionConstants.java
@@ -0,0 +1,8 @@
+package com.heimdallauth.server.constants;
+
+public class MongoCollectionConstants {
+    public static final String CONFIGURATION_SET_COLLECTION = "configuration-set-collection";
+    public static final String EMAIL_SUPPRESSION_LIST_COLLECTION = "email-suppression-list-collection";
+    public static final String EMAIL_ENTRY_COLLECTION = "email-entry-collection";
+    public static final String TRACKING_OPTIONS_COLLECTION = "tracking-options-collection";
+}
