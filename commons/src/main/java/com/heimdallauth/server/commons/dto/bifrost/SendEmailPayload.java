1adbcad3aaa7e1b7b3051066c71c02df23073ad3
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-01-27 23:41:22 -0700

    improve(SendEmailPayload): Implement the model classes and SendEmailPayload class for defining the API payload structure

---
+++ commons/src/main/java/com/heimdallauth/server/commons/dto/bifrost/SendEmailPayload.java
@@ -0,0 +1,18 @@
+package com.heimdallauth.server.commons.dto.bifrost;
+
+import com.heimdallauth.server.commons.models.bifrost.ConfigurationSet;
+import com.heimdallauth.server.commons.models.bifrost.RecipientModel;
+import com.heimdallauth.server.commons.models.bifrost.TemplateModel;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class SendEmailPayload {
+    private RecipientModel recipients;
+    private TemplateModel template;
+    private ConfigurationSet activeConfigurationSet;
+    private Object data;
+}
